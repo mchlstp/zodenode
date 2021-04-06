a1f81d1856112d92d8709c1b19990de042007f5c
mchlstp <michael.h.stump@gmail.com>
2021-04-06 00:28:10 -0500

    thumbnail

--- src/main/java/com/tts/ecom/Controllers/MainController.java
+++ src/main/java/com/tts/ecom/Controllers/MainController.java
@@ -2,7 +2,6 @@ package com.tts.ecom.Controllers;
 
 import java.util.List;
 import java.util.ArrayList;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
